9d7337521bda6445e4b2f88ec2db871cda4b6cb3
brian <riceb53@gmail.com>
2019-12-03 19:53:17 -0600

    add solutions of round 1 js

--- ruby_vs_js/practice.js
+++ ruby_vs_js/practice.js
@@ -8,7 +8,7 @@ function printNumbersDivisibleByThree() {
     index += 1;
   }
 }
-printNumbersDivisibleByThree();
+// printNumbersDivisibleByThree();
 
 
 // Write a method that accepts an array of strings and prints out every other string.
@@ -21,7 +21,7 @@ function printEveryOtherItem(strings) {
     index += 1;
   });
 }
-printEveryOtherItem(["a", "b", "c", "d", "e"]);
+// printEveryOtherItem(["a", "b", "c", "d", "e"]);
 
 
 // Write a method that accepts an array of numbers and returns the sum.
@@ -32,7 +32,7 @@ function computeSum(numbers) {
   });
   return sum;
 }
-console.log(computeSum([2, 4, 5]));
+// console.log(computeSum([2, 4, 5]));
 
 // Start with the hash: city_populations = {chi: 2700000}
 // Add populations to the city_populations hash for New York City (8.4 million) and San Francisco (800,000).
@@ -40,7 +40,7 @@ console.log(computeSum([2, 4, 5]));
 var cityPopulations = {chi: 2700000};
 cityPopulations["nyc"] = 8400000;
 cityPopulations.sf = 800000;  // same as cityPopulations["sf"]
-console.log(cityPopulations);
+// console.log(cityPopulations);
 
 
 
@@ -57,9 +57,28 @@ console.log(cityPopulations);
 // For example, if the initial array is ["a", "b", "c", "d", "e"], then the returned array should be ["a", "awesomesauce", "b", "awesomesauce", "c", "awesomesauce", "d", "awesomesauce", "e"].
 
 
+function awesomesauced(strings) {
+  // console.log(strings);
+  var newArray = [];
+  for (var i = 0; i < strings.length; i++) {
+    // console.log(strings[i]);
+    newArray.push(strings[i]);
+    newArray.push('awesomesauce');
+  }
+  newArray.pop();
+  console.log(newArray);
+}
+
+// awesomesauced(["a", "b", "c", "d", "e"]);
+
 // Start with the hash: item_amounts = {chair: 5, table: 2}
 // Someone just bought two chairs. Change the hash such that the chair amount is 3.
 // The final result should be: {chair: 3, table: 2}
+
+var itemAmounts = {chair: 5, table: 2};
+// itemAmounts["chair"] -= 2;
+itemAmounts.chair -= 2;
+// console.log(itemAmounts);
 
 
 // Start with the hash: item_amounts = {chair: 5, table: 2}
@@ -70,6 +89,30 @@ console.log(cityPopulations);
 // Write a method that accepts a number and returns its factorial.
 // For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
 
+function factorial(number) {
+  // console.log(number);
+  var product = 1;
+  while (number > 0) {
+    product *= number;
+    // console.log(number);
+    number--;
+  }
+  console.log(product);
+}
+
+// factorial(3);
 
 // Write a method that accepts two arrays of numbers, and prints the sum of every combination of numbers from first and second array.
 // For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should print a list: 101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
+
+function combinations(numbers1, numbers2) {
+  // console.log(numbers1, numbers2);
+  numbers1.forEach(function(number1) {
+
+    numbers2.forEach(function(number2) {
+      console.log(number1 + number2);
+    });
+  });
+}
+
+combinations([1, 5, 10], [100, 500, 1000]);
